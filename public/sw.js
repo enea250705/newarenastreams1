2c7f4b4c51dbca74eac3a44277f2f68a080660cf
enea250705 <enea250705@users.noreply.github.com>
2025-11-06 16:53:58 +0100

    Update service worker zoneId to 10150985

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,7 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10127777
+    "zoneId": 10150985
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
